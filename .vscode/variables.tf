51160f7cdfe6e29abdde935cbfa15e405557c65a
pceman <36867832+pceman@users.noreply.github.com>
2018-08-23 16:08:03 +0200

    middle of Lab3

---
+++ .vscode/variables.tf
@@ -0,0 +1,12 @@
+variable "loc" {
+    description = "Default Azure region"
+    default     =   "West Europe"
+}
+
+variable "tags" {
+    default     = {
+        source  = "citadel"
+        env     = "training"
+        Team    = "IT-SAAS-DTNA-Test"
+    }
+}
