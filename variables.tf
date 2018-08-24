1ce2966d9fd04c9fec68c3256b00e5e17900290a
pceman <36867832+pceman@users.noreply.github.com>
2018-08-24 11:42:21 +0200

    webapp created

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "loc" {
     description = "Default Azure region"
-    default     =   "West Europe"
+    default     =   "westeurope"
 }
 
 variable "tags" {
@@ -9,4 +9,22 @@ variable "tags" {
         env     = "training"
         Team    = "IT-SAAS-DTNA-Test"
     }
+}
+
+variable "tenant_id" {
+    # az ad sp show --id "http://terraformKeyVaultReader" --output tsv --query appOwnerTenantId
+    description = "GUID for the Azure AD tenancy or directory. "
+    default     = "4f4f4c56-a772-461a-967e-7890c3960b3a"
+}
+
+variable "kvr_object_id" {
+    # az ad sp show --id "http://terraformKeyVaultReader" --output tsv --query objectId}
+    description = "Object ID for the terraformKeyVaultReader service principal"
+    default     = "8c148645-4a91-4b81-b909-04cc09f721d5"
+}
+
+variable "webapplocs" {
+    description = "List of locations for web apps"
+    type        = "list"
+    default     = []
 }
